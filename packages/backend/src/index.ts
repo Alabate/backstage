1551a1a30b19ab3df436080dcbeccff9c0bc5a67
Aurelien Labate <aurelien.labate@cleverconnect.com>
2026-04-29 11:38:23 +0200

    chore: reproduct scaffolder gitlab push empty crash

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -61,6 +61,7 @@ backend.add(import('@backstage/plugin-permission-backend'));
 backend.add(import('@backstage/plugin-proxy-backend'));
 backend.add(import('@backstage/plugin-scaffolder-backend'));
 backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
+backend.add(import('@backstage/plugin-scaffolder-backend-module-gitlab'));
 backend.add(
   import('@backstage/plugin-scaffolder-backend-module-notifications'),
 );
